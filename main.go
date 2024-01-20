3afbd161a9e020de01964d7b7be37fe3f8bb8671
Jon Wright <jon@streamdal.com>
2024-01-19 21:30:56 -0800

    added cloudflare worker

--- main.go
+++ main.go
@@ -9,15 +9,22 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
+
 func main() {
-	token := os.Getenv("BOT_TOKEN")
+
+	err := godotenv.Load()
+	if err != nil {
+    log.Fatal("Error loading .env file")
+}
+	botToken := os.Getenv("BOT_TOKEN") // cloudflare worker
 	launchpadChannel := "1091545478576996453"
 	carlBot := "235148962103951360"
 	emuEmoji := "<:emu:1198109556987936909>"
 
-	sess, err := discordgo.New("Bot " + token)
+	sess, err := discordgo.New("Bot " + botToken)
 	if err != nil {
 		log.Fatal(err)
 	}
